61d7cbbd8fe69e6fb5934591bfab7f2d3f68ecd7
LahcenHaouch <lahcen.haouch@protonmail.com>
2024-03-28 23:47:58 +0100

    Revert pyodide loading from cdn

--- src/queries.ts
+++ src/queries.ts
@@ -1,9 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
-import { loadPyodide } from "pyodide";
 
 export function usePyodide() {
   return useQuery({
     queryKey: ["pyodide"],
-    queryFn: () => loadPyodide()
+    queryFn: loadPyodide
   })
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import {viteStaticCopy} from "vite-plugin-static-copy"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/local_python/",
-  plugins: [react(), viteStaticCopy({
-    targets: [
-      {
-        src: "node_modules/pyodide/*.*",
-        dest: "./pyodide"
-      }
-    ]
-  })],
-  optimizeDeps: {
-    exclude: ["pyodide"]
-  },
-  resolve: {
-    alias: {
-      "node-fetch": "isomorphic-fetch"
-    }
-  }
+  plugins: [react()]
 })
